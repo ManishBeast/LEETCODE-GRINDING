91dffce77d56e9989eb7ce5629cf482c44089a73
ManishBeast <MANISHRAJ0424@gmail.com>
2026-06-30 17:01:06 +0530

    Time: 1513 ms (34.96%), Space: 9.3 MB (75.05%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,23 +8,32 @@ public:
     }
     return true;
 }
-    int ans(string s,int i,vector<int> &dp){
-        if(i==s.size()){
-            return 0;
-        }
-        int minCost = INT_MAX;
-        if(dp[i]!=-1) return dp[i];
-        for(int j=i;j<s.size();j++){
-            if(isPalindrome(s,i,j)){
-                int cnt = 1+ans(s,j+1,dp);
-                minCost = min(minCost,cnt);
+    // int ans(string s,int i,vector<int> &dp){
+    //     if(i==s.size()){
+    //         return 0;
+    //     }
+    //     int minCost = INT_MAX;
+    //     if(dp[i]!=-1) return dp[i];
+    //     for(int j=i;j<s.size();j++){
+    //         if(isPalindrome(s,i,j)){
+    //             int cnt = 1+ans(s,j+1,dp);
+    //             minCost = min(minCost,cnt);
+    //         }
+    //     }
+    //     return dp[i]=minCost;
+    // }
+    int minCut(string s) {
+        vector<int> dp(s.size()+1,INT_MAX);
+        int n = s.size();
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            for(int j = i;j<n;j++){
+                if(isPalindrome(s,i,j)){
+                int cnt = 1+dp[j+1];
+                dp[i] = min(dp[i],cnt);
+                 }
             }
         }
-        return dp[i]=minCost;
-    }
-    int minCut(string s) {
-        vector<int> dp(s.size(),-1);
-        int n = s.size();
-        return ans(s,0,dp)-1;
+        return dp[0]-1;
     }
 };
