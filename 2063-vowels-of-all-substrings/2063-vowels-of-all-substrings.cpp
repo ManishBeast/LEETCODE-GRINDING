3f969ea2703420b788bb5b4291197622092425ba
ManishBeast <MANISHRAJ0424@gmail.com>
2026-06-30 15:22:46 +0530

    Time: 0 ms (100%), Space: 13.9 MB (65.4%) - LeetHub

---
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long countVowels(string word) {
+        long long ans = 0;
+        int n = word.size();
+        for (int i = 0; i < n; i++) {
+            char ch = word[i];
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                ans += 1LL * (i + 1) * (n - i);
+            }
+        }
+        return ans;
+    }
+};
