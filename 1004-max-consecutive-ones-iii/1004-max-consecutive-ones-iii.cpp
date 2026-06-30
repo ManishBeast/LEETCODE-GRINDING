3bc2bdd2d5d3636d4ccd690923be1800cdb49d21
ManishBeast <MANISHRAJ0424@gmail.com>
2026-06-30 12:53:39 +0530

    Time: 4 ms (22.08%), Space: 69.7 MB (27.18%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n = nums.size();
-        int left = 0;
         int right =0;
-        int zeros = 0;
-        int maxi = 0;
-        // bhai it is equivalent to longest subarray with only 2 zeros
+        int left =0;
+        int zeros =0;
+        int maxi =0;
         while(right<n){
             if(nums[right]==0){
                 zeros++;
             }
-            if(zeros>k){
+            while(zeros>k){
                 if(nums[left]==0){
                     zeros--;
                 }
                 left++;
             }
-
-           if(zeros<=k) maxi = max(maxi,right-left+1);
-        right++;
+            maxi = max(maxi,right-left+1);
+            right++;
         }
         return maxi;
     }
