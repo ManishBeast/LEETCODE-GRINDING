04e3ecdf965abc50c105e96fac49cce81aa837a8
ManishBeast <MANISHRAJ0424@gmail.com>
2026-07-01 10:44:04 +0530

    Time: 0 ms (100%), Space: 10.9 MB (41.21%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+           int n = s.size();
+        int maxLen =0;
+        int left =0;
+        int right =0;
+        int hash[26] = {0};
+        int maxFreq = 0;
+        while(right<n){
+                hash[s[right]-'A']++;
+                maxFreq = max(maxFreq,hash[s[right]-'A']);
+                int change = (right-left+1) - maxFreq;
+                while((right-left+1) - maxFreq>k){
+                    hash[s[left]-'A']--;
+                    left++;
+                }
+                maxLen = max(maxLen,right-left+1);
+                right++;
+        }
+        return maxLen;
+    }
+};
