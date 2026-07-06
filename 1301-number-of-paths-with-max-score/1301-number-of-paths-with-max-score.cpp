217ce2a2407928499706e45f6df0777392ef7dca
ManishBeast <MANISHRAJ0424@gmail.com>
2026-07-06 15:23:37 +0530

    Time: 44 ms (27.18%), Space: 44.4 MB (17.45%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -1,90 +1,42 @@
 class Solution {
-
-    int n;
-    int MOD = 1e9 + 7;
-    vector<vector<pair<int, int>>> t;
-
-    int getIntFromChar(char ch) {
-        return ch != 'S' ? ch - '0' : 0;
+public: 
+    vector<vector<pair<int,int>>>dp;
+    int mod =1e9+7;
+    int getInt(char ch){
+        return ch=='S'||ch=='E'?0:ch-'0';
     }
-
-    bool isValid(int i, int j, vector<string>& board) {
-        return i >= 0 && i < n && j >= 0 && j < n && board[i][j] != 'X';
+    bool isValid(int i,int j,vector<string>&board){
+        return j>=0 && i>=0 && i<board.size() && j<board[0].size() && board[i][j]!='X';
     }
-
-    pair<int, int> solve(int i, int j, vector<string>& board) {
-        if (board[i][j] == 'E') 
-            return {0, 1};   // reached end - no more coins, but found one path that reaches end - {0, 1}
-        if (board[i][j] == 'X') 
-            return {0, 0};   // dead end
-
-        if (t[i][j] != make_pair(-1, -1))
-            return t[i][j];
-
-        int upScore = 0,   upPaths = 0;
-        int leftScore = 0, leftPaths = 0;
-        int diagScore = 0, diagPaths = 0;
-        char ch = board[i][j];
-
-        if (isValid(i - 1, j, board)) {
-            auto [score, paths] = solve(i - 1, j, board);
-            upScore = score;
-            upPaths = paths;
-            if (upPaths > 0)
-                upScore += getIntFromChar(ch);
+    pair<int,int> solve(int i,int j,vector<string> &board,vector<vector<pair<int,int>>> &dp){
+        if(!isValid(i,j,board)){
+            return {0,0};
         }
-        if (isValid(i, j - 1, board)) {                 // move left
-            auto [score, paths] = solve(i, j - 1, board);
-            leftScore = score;
-            leftPaths = paths;
-            if (leftPaths > 0)
-                leftScore += getIntFromChar(ch);
+        if(board[i][j]=='E'){
+            return {0,1};
         }
-        if (isValid(i - 1, j - 1, board)) {             // move up-left (diagonal)
-            auto [score, paths] = solve(i - 1, j - 1, board);
-            diagScore = score;
-            diagPaths = paths;
-            if (diagPaths > 0)
-                diagScore += getIntFromChar(ch);
-        }
-
-        int bestScore, bestPaths;
-        if (upScore == leftScore && leftScore == diagScore) {
-            bestScore = upScore;
-            bestPaths = upPaths + leftPaths + diagPaths;
-        } else if (upScore == leftScore) {
-            bestScore = upScore;
-            bestPaths = upPaths + leftPaths;
-            if (diagScore > bestScore || (diagScore == bestScore && diagPaths > bestPaths)) {
-                bestScore = diagScore; bestPaths = diagPaths;
-            }
-        } else if (leftScore == diagScore) {
-            bestScore = leftScore;
-            bestPaths = leftPaths + diagPaths;
-            if (upScore > bestScore || (upScore == bestScore && upPaths > bestPaths)) {
-                bestScore = upScore; bestPaths = upPaths;
-            }
-        } else {
-            bestScore = upScore; bestPaths = upPaths;
-            if (leftScore > bestScore || (leftScore == bestScore && leftPaths > bestPaths)) {
-                bestScore = leftScore; bestPaths = leftPaths;
-            }
-            if (diagScore > bestScore || (diagScore == bestScore && diagPaths > bestPaths)) {
-                bestScore = diagScore; bestPaths = diagPaths;
+        if(dp[i][j]!=make_pair(-1,-1)) return dp[i][j];
+        int bestScore =-1;
+        long long bestPath = -1;
+        vector<pair<int,int>> prev = {solve(i-1,j,board,dp),solve(i-1,j-1,board,dp),solve(i,j-1,board,dp)};
+        for(auto[score,path]:prev){
+            if(path==0) continue;
+                score += getInt(board[i][j]);
+            if(score>bestScore){
+                bestScore = score;
+                bestPath = path;
+            }else if(score==bestScore){
+                bestPath = bestPath + path;
             }
         }
-
-        t[i][j] = {bestScore, bestPaths % MOD};
-        return t[i][j];
+        if(bestScore==-1) return dp[i][j] = {0,0};
+        return dp[i][j] = {bestScore,(int)(bestPath%mod)};
     }
-
-public:
     vector<int> pathsWithMaxScore(vector<string>& board) {
-        n = board.size();
-        
-        t.assign(n, vector<pair<int, int>>(n, {-1, -1}));
-
-        auto result = solve(n - 1, n - 1, board);
-        return {result.first, result.second};
+        int n = board.size();
+        int m = board[0].size();
+       dp.assign(n,vector<pair<int,int>>(m,{-1,-1}));
+        auto node = solve(n-1,m-1,board,dp); 
+        return {node.first,node.second};
     }
 };
