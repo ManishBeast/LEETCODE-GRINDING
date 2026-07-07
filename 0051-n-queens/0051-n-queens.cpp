139e9178035f42ac4831332b5588ba74fb39e363
ManishBeast <MANISHRAJ0424@gmail.com>
2026-07-07 15:08:58 +0530

    Time: 3 ms (62.12%), Space: 10.2 MB (78.96%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,63 +1,53 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string> &board, int n) {
-            // as we are putting from the left so we need to check in the left only that whether it is attacking it or not right that's it 
-    int duprow = row;
-    int dupcol = col;
-
-    // Check upper-left diagonal
-    while (row >= 0 && col >= 0) {
-        if (board[row][col] == 'Q')
-            return false;
-        row--;
-        col--;
+    bool check(int row,int col,vector<string> &board){
+        int n = board.size();
+        int m = board[0].size();
+        int rowE = row;
+        int colE = col;
+        while(colE>=0){
+            if(board[row][colE]=='Q') return false;
+            colE--;
+        }
+        colE=col;
+        while(rowE>=0 && colE>=0 ){
+            if(board[rowE][colE]=='Q') return false;
+            rowE--;
+            colE--;
+        }
+        rowE = row;
+        colE = col;
+        while(rowE<n && colE>=0){
+            if(board[rowE][colE]=='Q') return false;
+            rowE++;
+            colE--;
+        }
+        return true;
     }
 
-    row = duprow;
-    col = dupcol;
-
-    // Check left side
-    while (col >= 0) {
-        if (board[row][col] == 'Q')
-            return false;
-        col--;
-    }
-
-    row = duprow;
-    col = dupcol;
-
-    // Check lower-left diagonal
-    while (row < n && col >= 0) {
-        if (board[row][col] == 'Q')
-            return false;
-        row++;
-        col--;
-    }
-
-    return true;
-}
-    void nQueen(int col,vector<vector<string>> &ans,vector<string> &board,int n){
+    void ans(vector<string>&board,vector<vector<string>>&temp,int col){
+        int n = board.size();
         if(col==n){
-            ans.push_back(board);
-            return;
+            temp.push_back(board);
+            return ;
         }
-        for(int row=0;row<board.size();row++){
-            if(isSafe(row,col,board,n)){
-                board[row][col] ='Q';
-                nQueen(col+1,ans,board,n);
-                board[row][col] ='.';
+        for(int i=0;i<n;i++){
+            if(check(i,col,board)){
+                board[i][col]='Q';
+                ans(board,temp,col+1);
             }
+            board[i][col]='.';
         }
     }
+
     vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> temp;
         vector<string> board(n);
-        vector<vector<string>> ans;
         string s(n,'.');
-        for(int i=0;i<n;i++){
+        for(int i =0;i<n;i++){
             board[i]=s;
         }
-        nQueen(0,ans,board,n);
-        return ans;
-
+        ans(board,temp,0);
+        return temp;
     }
 };
