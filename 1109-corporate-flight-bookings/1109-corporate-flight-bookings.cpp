b163101403a4f442ecde53c8e396668e24649b6e
ManishBeast <MANISHRAJ0424@gmail.com>
2026-07-09 15:54:25 +0530

    Time: 4 ms (57.13%), Space: 72.1 MB (74.98%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> ans(n,0);
+        vector<int> diff(n,0);
         for(int i=0;i<bookings.size();i++){
             int l = bookings[i][0];
             int r = bookings[i][1];
-            for(int j=l;j<=r;j++){
-                ans[j-1]+=bookings[i][2]; // as given 1 bases index but we storing 0 based na so substract 1 bro 
-            }
+            int seats = bookings[i][2];
+
+            diff[l-1]+= seats;
+             if(r < n)
+                diff[r] -= seats;
         }
-        return ans;
+
+        for(int i=1;i<n;i++)
+            diff[i] += diff[i-1];
+
+        return diff;
     }
 };
